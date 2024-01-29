8e80025a3d96c6c31fe3d497deee9c369d411d1a
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-01-28 21:29:34 -0800

    Update listpage.js

--- list-page/listpage.js
+++ list-page/listpage.js
@@ -4,9 +4,6 @@ const createP = document.createElement('p');
 
 
 function saveWorkOut(){
-    createP.innerText = "it worked!!!!!";
+    createP.innerText = "you did " +repAmount.innerText + " " + workOutText.innerText + " in " + minutes + " minutes" ;
     savedWorkOuts.appendChild(createP);
-
-    alert("its working");
-
-}
+} 
